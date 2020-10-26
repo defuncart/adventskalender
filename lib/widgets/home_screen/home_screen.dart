c43bdd4dabe51be6de7d66a00cec071837f8c1ed
James Leahy <defuncart@gmail.com>
2020-10-26 15:24:53 +0100

    Add POC of database working.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -21,29 +21,39 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initFuture,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            if (snapshot.data) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text('Adventskalender'),
-                ),
-                body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Adventskalender'),
+      ),
+      body: FutureBuilder(
+        future: _initFuture,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              if (snapshot.data) {
+                return Stack(
                   children: [
                     Center(
-                      child: ElevatedButton(
-                        child: Text('Trigger Local Notification'),
-                        onPressed: () async =>
-                            await context.read(localNotificationServiceProvider).scheduleNotification(
-                                  id: 0,
-                                  title: 'Title',
-                                  body: 'Body',
-                                  scheduledDate:
-                                      context.read(dateTimeServiceProvider).localNow.add(const Duration(seconds: 5)),
-                                ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            child: Text('Trigger Local Notification'),
+                            onPressed: () async =>
+                                await context.read(localNotificationServiceProvider).scheduleNotification(
+                                      id: 0,
+                                      title: 'Title',
+                                      body: 'Body',
+                                      scheduledDate: context
+                                          .read(dateTimeServiceProvider)
+                                          .localNow
+                                          .add(const Duration(seconds: 5)),
+                                    ),
+                          ),
+                          ElevatedButton(
+                              child: Text('View db'),
+                              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => _DB()))),
+                        ],
                       ),
                     ),
                     Positioned(
@@ -53,22 +63,55 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     )
                   ],
-                ),
-              );
-            }
-            return Container();
-          default:
-            return Container();
-        }
-      },
+                );
+              }
+              return Container();
+            default:
+              return Container();
+          }
+        },
+      ),
     );
   }
 
   Future<bool> _init() async {
+    final db = context.read(databaseServiceProvider);
+    await db.initialize();
+
     final _notificationService = context.read(localNotificationServiceProvider);
     await _notificationService.initialize();
     await _notificationService.requestPermissions();
 
     return true;
+  }
+}
+
+class _DB extends StatelessWidget {
+  const _DB({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Consumer(
+        builder: (context, watch, child) {
+          final dbService = watch(databaseServiceProvider);
+
+          return ListView.builder(
+            itemCount: 24,
+            itemBuilder: (_, index) {
+              final dayIndex = index + 1;
+              final adventDay = dbService.getDay(dayIndex);
+
+              return ListTile(
+                leading: Image.asset('assets/database/$dayIndex.jpeg'),
+                title: Text(adventDay.title),
+                subtitle: Text(adventDay.description),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
