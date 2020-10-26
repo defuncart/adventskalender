382b2e1589ae4fa29debb8d31135813130e9a4ad
James Leahy <defuncart@gmail.com>
2020-10-26 16:33:04 +0100

    Add LoadingScreen to init app.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -1,93 +1,45 @@
 import 'package:adventskalender/di_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Future<bool> _initFuture;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _initFuture = _init();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Adventskalender'),
-      ),
-      body: FutureBuilder(
-        future: _initFuture,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              if (snapshot.data) {
-                return Stack(
-                  children: [
-                    Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            child: Text('Trigger Local Notification'),
-                            onPressed: () async =>
-                                await context.read(localNotificationServiceProvider).scheduleNotification(
-                                      id: 0,
-                                      title: 'Title',
-                                      body: 'Body',
-                                      scheduledDate: context
-                                          .read(dateTimeServiceProvider)
-                                          .localNow
-                                          .add(const Duration(seconds: 5)),
-                                    ),
-                          ),
-                          ElevatedButton(
-                              child: Text('View db'),
-                              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => _DB()))),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text(context.read(dateTimeServiceProvider).localNow.toString()),
-                      ),
-                    )
-                  ],
-                );
-              }
-              return Container();
-            default:
-              return Container();
-          }
-        },
-      ),
-    );
-  }
-
-  Future<bool> _init() async {
-    final dir = await getApplicationDocumentsDirectory();
-    Hive.init(dir.path);
-
-    final db = context.read(databaseServiceProvider);
-    await db.initialize(dir.path);
-
-    final _notificationService = context.read(localNotificationServiceProvider);
-    await _notificationService.initialize();
-    await _notificationService.requestPermissions();
-
-    return true;
+        appBar: AppBar(
+          title: Text('Adventskalender'),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: Text('Trigger Local Notification'),
+                    onPressed: () async => await context.read(localNotificationServiceProvider).scheduleNotification(
+                          id: 0,
+                          title: 'Title',
+                          body: 'Body',
+                          scheduledDate: context.read(dateTimeServiceProvider).localNow.add(const Duration(seconds: 5)),
+                        ),
+                  ),
+                  ElevatedButton(
+                      child: Text('View db'),
+                      onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => _DB()))),
+                ],
+              ),
+            ),
+            Positioned(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(context.read(dateTimeServiceProvider).localNow.toString()),
+              ),
+            )
+          ],
+        ));
   }
 }
 
---
+++ lib/widgets/loading_screen/loading_screen.dart
@@ -0,0 +1,54 @@
+import 'package:adventskalender/di_container.dart';
+import 'package:adventskalender/widgets/home_screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Future<bool> _initFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initFuture = _init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initFuture,
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.data) {
+            return HomeScreen();
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+
+  Future<bool> _init() async {
+    final dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+
+    final db = context.read(databaseServiceProvider);
+    await db.initialize(dir.path);
+
+    final _notificationService = context.read(localNotificationServiceProvider);
+    await _notificationService.initialize();
+    await _notificationService.requestPermissions();
+
+    return true;
+  }
+}
